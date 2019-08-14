2beacd6c6a289f14d52d22f60bf56f5e3974db90
GreyGao <rh87116938@163.com>
2019-08-14 22:36:39 +0800

    selection sort

---
+++ SelectionSort.py
@@ -0,0 +1,19 @@
+# find smallest item
+def findSmallest(arr):
+  smallest = arr[0]
+  smallest_index = 0
+  for i in range(1, len(arr)):
+    if arr[i] < smallest:
+      smallest = arr[i]
+      smallest_index = i
+  return smallest_index
+
+# select sort function
+def selectionSort(arr):
+  newArr = []
+  for i in range(len(arr)):
+    smallest = findSmallest(arr)
+    newArr.append(arr.pop(smallest))
+  return newArr
+
+print selectionSort([5,3,6,2,10])
